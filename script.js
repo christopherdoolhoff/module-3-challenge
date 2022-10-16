48e0603fa4e74b350ee8f29075cd764e57014f83
CDoolhoff <109476450+christopherdoolhoff@users.noreply.github.com>
2022-10-16 13:34:36 -0400

    created random fuctions for upper case letters, lower case letters, numbers, and symols.

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Random Upper Case Letter Function
+function randomUpperCaseLetter () {
+  return String.fromCharCode(Math.floor(Math.random()*26)+65);
+}
+
+// Random Lower Case Letter Function
+function randomLowerCaseLetter () {
+  return String.fromCharCode(Math.floor(Math.random()*26)+97);
+}
+
+// Random Number Function
+function randomNumber () {
+  return String.fromCharCode(Math.floor(Math.random()*10)+48);
+}
+
+// Random Symbol Function
+function randomSymbol () {
+  var symbols = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+  return symbols[Math.floor(Math.random()*symbols.length)];
+}
+
+// Functions test in console
+console.log(randomUpperCaseLetter());
+console.log(randomLowerCaseLetter());
+console.log(randomNumber());
+console.log(randomSymbol());
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
